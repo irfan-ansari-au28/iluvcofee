c288bac10a6c4329851bd3e9222b90cc1e0bfe0e
irfan-ansari-au28 <irfan398649@gmail.com>
2023-11-01 21:01:30 +0530

    create type orm entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeeService } from 'src/coffee/coffee.service';
+import { Coffee } from 'entity/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   controllers: [CoffeesController],
   providers: [CoffeeService],
 })
